01632dbe437987063154d1a55f2123052a7d6c95
Anri Liy <amedamachip713@gmail.com>
2020-04-02 10:37:37 -0700

    Add AddMovie

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MovieList from './MovieList';
 import Nav from './Nav';
 import './App.css';
 import {MovieProvider} from './MovieContext';
+import AddMovie from './AddMovie';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <MovieProvider>
       <div className="App">
         <Nav />
+        <AddMovie />
         <MovieList />
       </div>
     </MovieProvider>
